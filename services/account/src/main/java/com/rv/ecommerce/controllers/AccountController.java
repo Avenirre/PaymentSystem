cb48dc53d401b8be676c084aff4895fa9bea57bd
Roman <avenirre@gmail.com>
2026-03-31 22:30:14 +0300

    Add versioning + Swagger

--- services/account/src/main/java/com/rv/ecommerce/controllers/AccountController.java
+++ services/account/src/main/java/com/rv/ecommerce/controllers/AccountController.java
@@ -3,6 +3,12 @@ package com.rv.ecommerce.controllers;
 import com.rv.ecommerce.requests.AccountRequest;
 import com.rv.ecommerce.responses.AccountResponse;
 import com.rv.ecommerce.services.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +22,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/api/v1/account")
+@Tag(name = "Accounts", description = "Operations for bank accounts")
 @Slf4j
 @RequiredArgsConstructor
 public class AccountController {
@@ -25,6 +32,13 @@ public class AccountController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create account", description = "Creates a new account and returns its data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Account created",
+                    content = @Content(schema = @Schema(implementation = AccountResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Validation error"),
+            @ApiResponse(responseCode = "500", description = "Account creation failed")
+    })
     public AccountResponse createAccount(@Valid @RequestBody AccountRequest request) {
         log.info("POST /account called");
         AccountResponse response = accountService.createAccount(request);
@@ -33,6 +47,13 @@ public class AccountController {
     }
 
     @GetMapping("/{accountNumber}")
+    @Operation(summary = "Get account by number", description = "Returns account by account number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Account found",
+                    content = @Content(schema = @Schema(implementation = AccountResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Account not found"),
+            @ApiResponse(responseCode = "500", description = "Unexpected server error")
+    })
     public AccountResponse getAccountByAccountNumber(@PathVariable String accountNumber) {
         log.info("GET /account/{} called", accountNumber);
         AccountResponse response = accountService.getByAccountNumber(accountNumber);
